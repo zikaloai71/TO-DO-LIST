31226096a2a9f07f90d6e3d41bc41b328075a5b4
zikaloai71 <Ze.Lo.Moukhtar@el-eng.menofia.edu.eg>
2022-05-31 11:57:58 +0200

    different features added delete edit and complete

---
+++ src/deleteItem.js
@@ -0,0 +1,11 @@
+function deleteItem(){
+    let lists=[...document.querySelectorAll('.lists')]
+    let deleteButtons=[...document.querySelectorAll('.delete')];
+    deleteButtons.forEach((button)=>{
+        
+        button.addEventListener('click',()=>{
+           lists[deleteButtons.indexOf(button)].remove();
+        })
+    })
+    }
+    export{deleteItem}
