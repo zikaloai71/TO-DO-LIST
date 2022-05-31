2a84853ce686347ac1c92f8f58f21bf61a4a1d53
zikaloai71 <Ze.Lo.Moukhtar@el-eng.menofia.edu.eg>
2022-05-31 11:49:38 +0200

    set and create different to do items

---
+++ src/setItem.js
@@ -0,0 +1,37 @@
+import {title} from "./title";
+import { description } from "./description";
+import { dueDate } from "./due-date";
+import { priority } from "./priority";
+
+let ul = document.createElement('ul');
+let body = document.getElementsByTagName('body')[0];
+body.appendChild(ul);
+function Todo(title,description,date,priority){
+    this.title=title;
+    this.description=description;
+    this.date=date;
+    this.priority=priority;
+
+}
+function setItem(){
+let list =new Todo(title(),description(),dueDate(),priority());
+if(title()==''||description()==''||dueDate()==''){
+    return;
+}
+return list;
+}
+
+function createItem(list){
+    let li= document.createElement('li');
+    ul.appendChild(li);
+    li.classList.add('lists');
+    li.innerHTML+=`<p> <span>title :  ${list.title}</span>
+    <span> description :  ${list.description}</span>
+     <span>due-date : ${list.date}</span></p>
+    <aside>
+    <button class="delete"><i class="fa-solid fa-trash"></i></button><button class='edit'><i class="fa-solid fa-pen-to-square"></i></button><button class='complete'><i class="fa-solid fa-check"></i></button></aside> 
+    `;
+    li.style.backgroundColor=list.priority;
+}
+
+export{setItem,createItem}
